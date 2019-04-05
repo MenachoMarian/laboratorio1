0d934c554bc2e12920ab0e155c3ed09c1d24d4f1
MenachoMarian <marian.menacho369@gmail.com>
2019-04-04 22:20:44 -0400

    ejecución chackPalindrome exitosa

---
+++ solutions/checkPalindrome.js
@@ -0,0 +1,20 @@
+function checkPalindrome(cadena){
+  var count = cadena.length;
+  var nuevap="";
+
+  while(count>-1){
+    nuevap = nuevap + cadena.charAt(count);
+    count--;
+  }
+  if(nuevap===cadena)
+  {
+    return "true";
+  }
+  else{
+    return "false";
+  }
+  //return nuevap;
+}
+
+var inputString="a";
+console.log(checkPalindrome(inputString));
