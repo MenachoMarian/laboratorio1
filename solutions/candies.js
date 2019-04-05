eb5b64b0c0806f9c05837d5ddb68fff18e8e0fdd
Yessica <yessnicolerp@gmail.com>
2019-04-04 23:51:57 -0400

    prueba3

--- solutions/candies.js
+++ solutions/candies.js
@@ -4,10 +4,10 @@ function candies(n,m) {
     if (m>=n) {
       r = m/n;
        r= parseInt(r);
-
       return r;
-    }
+    }else
+    document.write('dulces insuficientes');
 }
 
 //module.exports = candies
-console.log(candies(3,));
+console.log(candies(3,6));
