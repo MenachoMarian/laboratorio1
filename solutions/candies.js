7cc75996780bd1028ef4168e0d8b6cb0eade774d
MenachoMarian <marian.menacho369@gmail.com>
2019-04-05 12:46:25 -0400

    corriendo candies (2 pruebas no pass)

--- solutions/candies.js
+++ solutions/candies.js
@@ -1,12 +1,14 @@
 function candies(n,m) {
   var resp = 0;
     if (m>=n) {
-      resp = m/n;
-       resp= parseInt(resp);
-      return resp;
+      resp = Math.trunc(m/n);
+       //resp= parseInt(resp);
+      return resp*n;
     }else
-    document.write('dulces insuficientes');
+  {
+    return "dulces insuficientes";
+  }
 }
 
 module.exports = candies
-//console.log(candies(3,6));
+//console.log(candies(8,2));
